50bb5b687982de6cf6d4c6b01935851526f1e6d3
Robert Krahn <robert.krahn@gmail.com>
2014-08-13 05:09:16 +0000

    log file settings

--- lively/start.sh
+++ lively/start.sh
@@ -5,6 +5,8 @@ cd /home/lively/lively-web.org/lively
 port=9001
 container_name=lively-web-server
 lively_host_dir=/home/lively/lively-web.org/data/LivelyKernel
+log_dir=/home/lively/lively-web.org/logs
+expt_dir=/home/lively/expt
 
 docker_procs=$(docker ps | grep ":$port" | awk '{ print $1 }')
 [ -n "$docker_procs" ] && xargs docker stop
@@ -14,8 +16,12 @@ docker_procs=$(docker ps | grep ":$port" | awk '{ print $1 }')
 docker run \
     --rm \
     -v $lively_host_dir:/home/lively/LivelyKernel \
+    -v $log_dir:/home/lively/logs \
+    -v $expt_dir:/home/lively/expt \
     -p 8080:9001 \
     -p 9002:9002 \
     -p 9003:9003 \
     -p 9004:9004 \
-    -i -t $container_name
+    $container_name
+
+echo "[$(date -R)] Docker exited with $?"
