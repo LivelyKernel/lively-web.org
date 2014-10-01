43faf5cc07566be42fea0e28b05dd8fcd3e74aa9
Robert Krahn <robert.krahn@gmail.com>
2014-10-01 00:49:29 +0000

    have docker exit when process comes down (+ kill it if it didn't)

--- lively/start.sh
+++ lively/start.sh
@@ -8,8 +8,13 @@ lively_host_dir=/home/lively/lively-web.org/data/LivelyKernel
 log_dir=/home/lively/lively-web.org/logs
 expt_dir=/home/lively/expt
 
-docker_procs=$(docker ps | grep ":$port" | awk '{ print $1 }')
-[ -n "$docker_procs" ] && xargs docker stop
+docker_procs=$(docker ps | grep "$port/tcp" | awk '{ print $1 }')
+if [ -n "$docker_procs" ]; then
+    echo "docker for container $container_name already running ($docker_procs)... stopping it..."
+    sleep 1
+    docker stop $docker_procs
+    sleep 1
+fi
 
 [ -z "$(docker ps -a | grep $container_name)" ] && docker build --rm -t $container_name .
 
@@ -22,6 +27,7 @@ docker run \
     -p 9002:9002 \
     -p 9003:9003 \
     -p 9004:9004 \
+    -t \
     $container_name
 
 echo "[$(date -R)] Docker exited with $?"
