9975c29ccf928309be06de7878b14d93ca913e67
Robert Krahn <robert.krahn@gmail.com>
2016-01-19 04:28:16 +0000

    cert stuff cleanup

--- http/ssl/register-cert.sh
+++ http/ssl/register-cert.sh
@@ -13,27 +13,44 @@ client_password=$4
 # ca_loc=/home/lively/lively-web.org/http/ssl/lively-web-CA
 # username="robertkrahn"
 # email="robert.krahn@gmail.com"
-# client_password="tebor1"
+# client_password="...."
+# ./register-cert.sh /home/lively/lively-web.org/http/ssl/lively-web-CA robertkrahn "robert.krahn@gmail.com" ****
+
+echo -e "Creating private key file for ${username} of CA ${ca_loc}"
+
+mkdir -p $ca_loc
+mkdir -p $ca_loc/private
+mkdir -p $ca_loc/csr
+mkdir -p $ca_loc/certs
 
 cd $ca_loc;
 
+if [[ ! -f "$ca_loc/openssl.cnf" ]]; then
+  echo "No $ca_loc/openssl.cnf exists, see /etc/ssl/openssl.cnf for an example."
+  exit 1;
+fi
+
+echo "1. generating private/client_$username.key"
 openssl genrsa -des3 \
     -passout pass:"$client_password" \
     -out "private/client_$username.key" 2048
  
 # Remove passphrase 
+echo "2. removing passphrase from private/client_$username.key"
 openssl rsa \
     -passin pass:"$client_password" \
     -in "private/client_$username.key" \
     -out private/client_$username.key
  
 # Create CSR for the client.
+echo "3. create client csr csr/client_$username.csr"
 openssl req -config openssl.cnf -new \
     -subj "/C=US/L=San Francisco/O=livelyweb-internal/CN=$username/emailAddress=$email" \
     -key "private/client_$username.key" \
     -out "csr/client_$username.csr"
 
 # Create client certificate.
+echo "4. create client certificate. certs/client_$username.crt"
 openssl ca -batch -config openssl.cnf \
     -days 365 \
     -in "csr/client_$username.csr" \
@@ -42,6 +59,7 @@ openssl ca -batch -config openssl.cnf \
 
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 # 5. Export the client certificate to pkcs12
+echo "5. export the client certificate to pkcs12 certs/client_$username.p12"
 openssl pkcs12 -export \
     -passout pass:"$client_password" \
     -in "certs/client_$username.crt" \
